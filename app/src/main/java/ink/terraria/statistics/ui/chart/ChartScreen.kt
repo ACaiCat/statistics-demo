41ec898bd2cad05291a7477d3913182ffce0c8e1
ACaiCat <13110818005@qq.com>
2026-05-03 04:22:00 +0800

    feat: add button group to switch chart

--- app/src/main/java/ink/terraria/statistics/ui/chart/ChartScreen.kt
+++ app/src/main/java/ink/terraria/statistics/ui/chart/ChartScreen.kt
@@ -1,21 +1,36 @@
 package ink.terraria.statistics.ui.chart
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.BarChart
+import androidx.compose.material.icons.filled.StackedLineChart
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.FilledIconToggleButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -66,13 +81,57 @@ fun ChartBody(
     items: List<Item>,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
-        OutlineLineChart(
-            data = items, height = 300.dp, modifier = Modifier.padding(vertical = 16.dp)
-        )
-        OutlineBarChart(
-            data = items, modifier = Modifier.padding(vertical = 16.dp)
-        )
+    var barChart by remember { mutableStateOf(true) }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Spacer(modifier = Modifier.padding(vertical = 40.dp))
+
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            TextButton(
+                enabled = !barChart,
+                onClick = {
+                    barChart = true
+                },
+                colors = ButtonDefaults.textButtonColors(
+                    disabledContentColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            ) {
+                Icon(Icons.Default.BarChart, stringResource(R.string.bar_chart))
+                Text(stringResource(R.string.bar_chart))
+            }
+            TextButton(
+                enabled = barChart,
+                onClick = {
+                    barChart = false
+                },
+                colors = ButtonDefaults.textButtonColors(
+                    disabledContentColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            ) {
+                Icon(Icons.Default.StackedLineChart, stringResource(R.string.line_chart))
+                Text(stringResource(R.string.line_chart))
+            }
+        }
+
+        if (barChart) {
+            OutlineBarChart(
+                data = items, modifier = Modifier.padding(vertical = 16.dp)
+            )
+        } else {
+            OutlineLineChart(
+                data = items, height = 300.dp, modifier = Modifier.padding(vertical = 16.dp)
+            )
+        }
+
     }
 }
 
